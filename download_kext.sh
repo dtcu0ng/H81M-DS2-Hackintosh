2b3822534c3576aaaa0b6f4e541677f77cbdaf8c
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-05-30 01:39:14 +0700

    caps

--- download_kext.sh
+++ download_kext.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 download_kext () {
-    echo Downloading $kext_name
-    RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$full_kext_name/releases/latest)
+    echo Downloading $KEXT_NAME
+    RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$FULL_KEXT_NAME/releases/latest)
     TAG="${RELEASE_URL##*/}"
-    url=https://github.com/$full_kext_name/releases/download/$TAG/$kext_name-$TAG-RELEASE.zip
+    url=https://github.com/$FULL_KEXT_NAME/releases/download/$TAG/$KEXT_NAME-$TAG-RELEASE.zip
     curl -# -L -O "${url}" || exit 1
-    unzip -qq "$kext_name-$TAG-RELEASE.zip" || exit 1
+    unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
 }
 
-export full_kext_name = "acidanthera/VirtualSMC"
-export kext_name = "VirtualSMC"
+FULL_KEXT_NAME="acidanthera/VirtualSMC"
+KEXT_NAME="VirtualSMC"
 download_kext
 
-export full_kext_name = "acidanthera/WhateverGreen"
-export kext_name = "WhateverGreen"
+FULL_KEXT_NAME="acidanthera/WhateverGreen"
+KEXT_NAME="WhateverGreen"
 download_kext
