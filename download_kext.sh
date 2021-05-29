a11825f5806d568921d6f1f3ccd12e54afd59dcf
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-05-30 02:18:50 +0700

    test out

--- download_kext.sh
+++ download_kext.sh
@@ -5,33 +5,35 @@ download_kext_gh () {
     url=https://github.com/$FULL_KEXT_NAME/releases/download/$TAG/$KEXT_NAME-$TAG-RELEASE.zip
     echo Downloading $KEXT_NAME v$TAG
     curl -# -L -O "${url}" || exit 1
+    unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
 }
 
 virtualsmc_download() {
     FULL_KEXT_NAME="acidanthera/VirtualSMC"
     KEXT_NAME="VirtualSMC"
     download_kext_gh
-    unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
 }
 
 whatevergreen_download() {
     FULL_KEXT_NAME="acidanthera/WhateverGreen"
     KEXT_NAME="WhateverGreen"
     download_kext_gh
-    unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
 }
 
 lilu_download() {
     FULL_KEXT_NAME="acidanthera/lilu"
     KEXT_NAME="Lilu"
     download_kext_gh
-    unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
 }
 
 realtek8111_download() {
     FULL_KEXT_NAME="Mieze/RTL8111_driver_for_OS_X"
     KEXT_NAME="RealtekRTL8111"
-    download_kext_gh
+    RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$FULL_KEXT_NAME/releases/latest)
+    TAG="${RELEASE_URL##*/}"
+    url=https://github.com/$FULL_KEXT_NAME/releases/download/$TAG/$KEXT_NAME-V$TAG.zip
+    echo Downloading $KEXT_NAME v$TAG
+    curl -# -L -O "${url}" || exit 1
     unzip -qq "$KEXT_NAME-V$TAG.zip" || exit 1
 }
 
@@ -39,7 +41,6 @@ applealc_download() {
     FULL_KEXT_NAME="acidanthera/AppleALC"
     KEXT_NAME="AppleALC"
     download_kext_gh
-    unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
 }
 
 usbinjectall_download() {
