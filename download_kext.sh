6893844a3fb4f286cca3aaf9c57204f422b1e63d
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-05-30 16:00:23 +0700

    Update download_kext.sh

--- download_kext.sh
+++ download_kext.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-download_kext_gh () {
+prepare() {
+    mkdir DownloadedKexts
+    cd DownloadedKexts
+}
+download_kext_gh() {
     RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$FULL_KEXT_NAME/releases/latest)
     TAG="${RELEASE_URL##*/}"
     url=https://github.com/$FULL_KEXT_NAME/releases/download/$TAG/$KEXT_NAME-$TAG-RELEASE.zip
     echo Downloading $KEXT_NAME v$TAG
     curl -# -L -O "${url}" || exit 1
     unzip -qq "$KEXT_NAME-$TAG-RELEASE.zip" || exit 1
+    rm "$KEXT_NAME-$TAG-RELEASE.zip" # clean up
+}
+
+download_kext_gh_custom() {  # for custom kexts are not have filename formatted with $KEXT_NAME-$TAG-$TARGET.zip or outside GitHub
+    echo Downloading $KEXT_NAME
+    curl -# -L -O "${url}" || exit 1
+    unzip -qq "$KEXT_FILENAME.zip" || exit 1
+    rm "$KEXT_FILENAME.zip"
 }
 
 virtualsmc_download() {
@@ -31,10 +43,9 @@ realtek8111_download() {
     KEXT_NAME="RealtekRTL8111"
     RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$FULL_KEXT_NAME/releases/latest)
     TAG="${RELEASE_URL##*/}"
-    url=https://github.com/$FULL_KEXT_NAME/releases/download/$TAG/$KEXT_NAME-V$TAG.zip
+    KEXT_FILENAME="RealtekRTL8111-$TAG"
+    url=https://github.com/$FULL_KEXT_NAME/releases/download/$TAG/$KEXT_FILENAME.zip
     echo Downloading $KEXT_NAME v$TAG
-    curl -# -L -O "${url}" || exit 1
-    unzip -qq "$KEXT_NAME-V$TAG.zip" || exit 1
 }
 
 applealc_download() {
@@ -44,21 +55,20 @@ applealc_download() {
 }
 
 usbinjectall_download() {
-    echo Downloading RehabMan-USBInjectAll-2018-1108.zip
-    curl -# -L -O "https://bitbucket.org/RehabMan/os-x-usb-inject-all/downloads/RehabMan-USBInjectAll-2018-1108.zip" || exit 1
-    unzip -qq "RehabMan-USBInjectAll-2018-1108.zip" || exit 1
+    url="https://bitbucket.org/RehabMan/os-x-usb-inject-all/downloads/RehabMan-USBInjectAll-2018-1108.zip"
+    KEXT_NAME="USBInjectAll"
+    KEXT_FILENAME="RehabMan-USBInjectAll-2018-1108"
+    download_kext_gh_custom
 }
 
 main(){
-    mkdir DownloadedKexts
-    cd DownloadedKexts
+    prepare
     virtualsmc_download
     whatevergreen_download
     lilu_download
     realtek8111_download
     applealc_download
     usbinjectall_download
-    ls
 }
 
 main
