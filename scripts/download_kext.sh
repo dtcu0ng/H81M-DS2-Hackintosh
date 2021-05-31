13a62b04387f955a70f6765e72c856020d4d8f7e
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-05-31 15:06:22 +0700

    update

--- scripts/download_kext.sh
+++ scripts/download_kext.sh
@@ -3,7 +3,7 @@ prepare() {
     TARGET="Release"
     [ ! -d "DownloadedKexts" ] && mkdir DownloadedKexts
     cd DownloadedKexts
-    echo "Installed kexts in CI#$GITHUB_RUN_NUMBER for commit $GITHUB_SHA:" >> installed_kext.txt
+    echo "Installed kexts in CI#$GITHUB_RUN_NUMBER for commit $GITHUB_SHA:" >> EFI/OC/installed_kext.txt
 }
 download_kext_gh() {
     RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$FULL_KEXT_NAME/releases/latest)
@@ -13,7 +13,7 @@ download_kext_gh() {
     curl -# -L -O "${url}" || exit 1
     unzip -qq "$KEXT_NAME-$TAG-$TARGET.zip" || exit 1
     rm "$KEXT_NAME-$TAG-$TARGET.zip" # clean up
-    echo "$KEXT_NAME ($TARGET) version $TAG" >> installed_compoments.txt
+    echo "$KEXT_NAME ($TARGET) version $TAG" >> EFI/OC/installed_compoments.txt
 }
 
 download_kext_gh_custom() {  # for custom kexts are not have filename formatted with $KEXT_NAME-$TAG-$TARGET.zip or outside GitHub
@@ -21,7 +21,7 @@ download_kext_gh_custom() {  # for custom kexts are not have filename formatted 
     curl -# -L -O "${url}" || exit 1
     unzip -qq "$KEXT_FILENAME.zip" || exit 1
     rm "$KEXT_FILENAME.zip"
-    echo "$KEXT_NAME ($TARGET) version $TAG" >> installed_compoments.txt
+    echo "$KEXT_NAME ($TARGET) version $TAG" >> EFI/OC/installed_compoments.txt
 }
 
 virtualsmc_download() {
@@ -77,7 +77,6 @@ copy_kext() {
     cp -R DownloadedKexts/Lilu.kext EFI/OC/Kexts/Lilu.kext
     cp -R DownloadedKexts/$TARGET/USBInjectAll.kext EFI/OC/Kexts/USBInjectAll.kext
     cp -R DownloadedKexts/RealtekRTL8111-V$RTLTAG/$TARGET/RealtekRTL8111.kext EFI/OC/Kexts/RealtekRTL8111.kext
-    cp DownloadedKexts/installed_compoments.txt EFI/OC
 }
 
 cleanup() {
