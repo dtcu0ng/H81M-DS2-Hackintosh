a9fd28e4aa6b8f992fc6b29e499884059f940f92
Cuong Tien Dinh <me@ctdinh.io.vn>
2023-08-16 20:26:32 +0700

    scripts: thank you Bing Chat

--- scripts/BuildOC.sh
+++ scripts/BuildOC.sh
@@ -7,11 +7,11 @@
 #==============================================================
 
 checkTarget() {
-    if echo $TARGET = "DEBUG" -o echo $TARGET = "RELEASE"; then
+    if [ "$TARGET" == "DEBUG" ] || [ "$TARGET" == "RELEASE" ]; then
         echo Found valid target: $TARGET
         echo "buildTarget=${TARGET}" >> $GITHUB_OUTPUT
     else
-        echo Unvaild target: $TARGET
+        echo Invaild target: $TARGET
         exit 1
     fi
 }
