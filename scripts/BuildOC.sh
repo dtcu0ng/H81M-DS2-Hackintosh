55fdf0937db7ff9a8682c8bc4497c4283030b412
Cuong Tien Dinh <me@ctdinh.io.vn>
2023-08-16 19:47:15 +0700

    scripts: simplified target detection method

--- scripts/BuildOC.sh
+++ scripts/BuildOC.sh
@@ -7,10 +7,7 @@
 #==============================================================
 
 checkTarget() {
-    if [ "$TARGET" == "DEBUG" ]; then
-        echo Found valid target: $TARGET
-        echo "buildTarget=${TARGET}" >> $GITHUB_OUTPUT
-    elif [ "$TARGET" == "RELEASE" ]; then
+    if [[ "$TARGET" == "DEBUG" || "TARGET" == "RELEASE" ]]; then
         echo Found valid target: $TARGET
         echo "buildTarget=${TARGET}" >> $GITHUB_OUTPUT
     else
